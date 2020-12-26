be4e284ff3b9f3eebba501c88ddf3b13d075810f
Bar Lis <48059962+bar2693lis@users.noreply.github.com>
2020-12-26 22:01:32 +0200

    first commit

---
+++ Drum Kit/index.js
@@ -0,0 +1,54 @@
+
+// Detecting Button Press
+var btnArrSize = document.querySelectorAll(".drum").length;
+for(var i=0; i<btnArrSize; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+      var buttonInnerHTML = this.innerHTML;
+      makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function bla(event){
+  var buttonKeydown = event.key;
+  makeSound(buttonKeydown);
+  buttonAnimation(buttonKeydown);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+
+    default: console.log(buttonInnerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
